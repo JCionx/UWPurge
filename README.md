6577ac0700ad11217df9697d3fb2d5133427474d
JCionx <jcionx0@gmail.com>
2024-09-17 15:06:43 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# UWPurge
+
+UWPurge is a PowerShell script that removes almost all Windows UWP apps, and replaces them with faster open-source alternatives.
+
+![](src/1.png)
+
+Windows 11's built-in apps are built on the UWP framwork. This framework allows developers to build beautiful apps, but at the cost of performance.By replacing the Windows UWP apps with fast Win32 apps, the Windows experience gets much snappier, specially on weaker hardware.
+
+UWPurge also features a custom taskbar and start menu, because the default taskbar and start menu have UWP components as well.
+
+![](src/2.png)
+
+Tested on:
+- Windows 11 Pro
+
+## Installing
+Open **PowerShell** as Administrator (not the Windows terminal) and run this:
+```powershell
+irm https://raw.githubusercontent.com/JCionx/UWPurge/master/UWPurge.ps1 | iex
+```
+
+## Credits
+Projects used in UWPurge:
+- [Win11Debloat](https://github.com/Raphire/Win11Debloat)
+- [Open-Shell](https://github.com/Open-Shell/Open-Shell-Menu)
+- [ExplorerPatcher](https://github.com/valinet/ExplorerPatcher)
---
+++ UWPurge.ps1
@@ -0,0 +1,85 @@
+Add-Type -AssemblyName PresentationCore,PresentationFramework
+
+cd $env:UserProfile
+
+
+# Enable winget
+
+Write-Host -ForegroundColor Cyan "[#] Downloading WinGet package"
+$ProgressPreference = 'SilentlyContinue'
+Invoke-WebRequest https://aka.ms/getwinget -OutFile winget.msixbundle
+
+Write-Host -ForegroundColor Cyan "[#] Installing WinGet package..."
+Add-AppxPackage -Path ".\winget.msixbundle"
+
+Write-Host -ForegroundColor Cyan "[#] Removing WinGet package..."
+Remove-Item ".\winget.msixbundle"
+
+
+# Debloat
+Write-Host -ForegroundColor Cyan "[#] Cloning Win11Debloat repository..."
+Invoke-WebRequest https://github.com/Raphire/Win11Debloat/archive/refs/heads/master.zip -OutFile win11debloat.zip
+
+Write-Host -ForegroundColor Cyan "[#] Decompressing Win11Debloat repository archive..."
+Expand-Archive -Path win11debloat.zip
+
+Write-Host -ForegroundColor Cyan "[#] Removing Win11Debloat repository archive..."
+Remove-Item ".\win11debloat.zip"
+
+cd ".\win11debloat\Win11Debloat-master"
+
+Write-Host -ForegroundColor Cyan "[#] Downloading CustomAppsList file..."
+Invoke-WebRequest https://raw.githubusercontent.com/JCionx/UWPurge/master/assets/CustomAppsList -OutFile CustomAppsList
+
+Write-Host -ForegroundColor Cyan "[#] Running Win11Debloat script..."
+.\Win11Debloat.ps1 -HideGallery -DisableRecall -DisableCopilot -DisableWidgets -HideChat -HideTaskview -HideSearchTb -TaskbarAlignLeft -ShowKnownFileExt -DisableSuggestions -DisableBing -DisableTelemetry -ClearStartAllUsers -DisableDVR -RemoveGamingApps -RemoveDevApps -RemoveW11Outlook -RemoveCommApps -RemoveAppsCustom -ForceRemoveEdge -Silent
+Write-Host -ForegroundColor Cyan "[#] Removing UWP notepad..."
+Get-AppxPackage *Microsoft.WindowsNotepad* | Remove-AppxPackage
+cd ../../
+
+Write-Host -ForegroundColor Cyan "[#] Removing Win11Debloat repository..."
+Remove-Item .\win11debloat -Recurse
+
+
+# Install apps
+
+Write-Host -ForegroundColor Cyan "[#] Installing 7-Zip..."
+winget install 7zip
+
+Write-Host -ForegroundColor Cyan "[#] Installing VLC..."
+winget install VideoLAN.VLC
+
+Write-Host -ForegroundColor Cyan "[#] Installing ExplorerPatcher..."
+winget install valinet.ExplorerPatcher
+
+Write-Host -ForegroundColor Cyan "[#] Installing OpenShell..."
+winget install --id "Open-Shell.Open-Shell-Menu" --source winget --silent --custom 'ADDLOCAL=StartMenu'
+
+
+# Setup ExplorerPatcher config
+
+cd $env:UserProfile/Desktop
+
+Write-Host -ForegroundColor Cyan "[#] Downloading ExplorerPatcher config file..."
+Invoke-WebRequest https://raw.githubusercontent.com/JCionx/UWPurge/master/assets/ExplorerPatcherConfig.reg -OutFile ExplorerPatcherConfig.reg
+
+Write-Host ""
+Write-Host -ForegroundColor Red "[!] Load the ExplorerPatcher config!"
+Write-Host -ForegroundColor Red "1. Right click on the taskbar"
+Write-Host -ForegroundColor Red "2. Click on Properties"
+Write-Host -ForegroundColor Red "3. Click on Settings and uninstall"
+Write-Host -ForegroundColor Red "4. Click on Import Settings"
+Write-Host -ForegroundColor Red "5. Navigate to your desktop and choose the ExplorerPatcherConfig.reg file"
+pause
+
+Write-Host -ForegroundColor Cyan "[#] Removing ExplorerPatcher config file..."
+Remove-Item .\ExplorerPatcherConfig.reg
+
+Write-Host -ForegroundColor Cyan "[#] Restarting explorer..."
+taskkill /F /IM explorer.exe
+Start explorer.exe
+
+Write-Host ""
+Write-Host -ForegroundColor Cyan "Script complete!"
+
+pause
